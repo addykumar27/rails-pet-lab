a2ecdaeaa034ecd3860f84c419dd2b202168fee1
addykumar27 <adarshna27@yahoo.com>
2017-05-22 22:07:30 -0700

    adding models for pets and owner with validation

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,15 +1,22 @@
 class Owner < ActiveRecord::Base
-  # TODO: add association ot pets
-  
-  # TODO: add association to appointments (through pets)
-  
-  # TODO: add validations
+   	has_many :pets, dependent: :destroy
 
-  before_save :normalize_phone_number
+	before_save :normalize_phone_number
+
+	validates :first_name,
+            presence:   true,
+            length:   { maximum: 255 }
+
+ 	validates :last_name,
+            presence:   true,
+            length:   { maximum: 255 }
 
   # removes leading 1 and the characters (, ), -, .
   def normalize_phone_number
-    # stretch
+       if phone.present?
+      phone.gsub!(/^1/, "")
+      phone.gsub!(/[()-.]/,"")
+    	end
   end
 
 end
